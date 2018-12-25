b38bd1e22f90068b7c73bf7a9143c551b6c2cab7
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-25 01:30:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,13 +27,10 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  # return the shipping_manifest hash below 
+  end
+  shipping_manifest
 end
